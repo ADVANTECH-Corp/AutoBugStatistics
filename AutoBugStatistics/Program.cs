1798398436638861b6e50ce5b8990c64f08a188f
lijie-advantech <1016494666@qq.com>
2017-08-24 13:37:05 +0800

    Update Program.cs

--- AutoBugStatistics/Program.cs
+++ AutoBugStatistics/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +26,9 @@ namespace AutoBugStatistics
         private static RedmineFun redmine = new RedmineFun();        
         private static Dictionary<string, int> BugNumber = new Dictionary<string, int>();
         private static Dictionary<string, string> LogList = new Dictionary<string, string>();
-        private static int MaxStrLenght = 13;
+        private static Dictionary<string, int> StatusList = new Dictionary<string, int>();
+        private static Dictionary<string, int> PriorityList = new Dictionary<string, int>();
+        
         static void Main(string[] args)
         {
             string filePath = System.Environment.CurrentDirectory + "\\BugStatistic_" +
@@ -45,22 +47,33 @@ namespace AutoBugStatistics
                     return;
                 }
 
+                foreach (IssueStatus itemStatus in redmine.GetAllObjectList<IssueStatus>())
+                {
+                    if (!StatusList.ContainsKey(itemStatus.Name) && itemStatus.Name.ToLower() != "closed")
+                        StatusList.Add(itemStatus.Name, 0);
+                }
+                foreach (IssuePriority itemPriority in redmine.GetAllObjectList<IssuePriority>())
+                {
+                    if (!PriorityList.ContainsKey(itemPriority.Name))
+                        PriorityList.Add(itemPriority.Name, 0);
+                }
+
                 foreach (Project item in redmine.GetAllObjectList<Project>())
                 {
                     try
                     {
-                        BugNumber.Clear();
+                        BugNumber.Clear();                       
                         if (IsProjectExistInIni(item))
                         {
-                            Console.WriteLine("Statistic " + item.Name + "...");
+                            Console.WriteLine("Statistic " + item.Name + "...");     
                             NameValueCollection mem = new NameValueCollection { { "project_id", item.Id.ToString() }, { "status_id", "*" } };
-                            int num = 0;
+                            int num = 0;   
                             foreach (Issue itemissue in redmine.GetAllObjectList<Issue>(mem, out num))
                             {
                                 if (itemissue.Tracker.Name.ToLower() == "bug")
                                 {
-                                    string status = itemissue.Status.Name.ToLower();
-                                    string priority = itemissue.Priority.Name.ToLower();
+                                    string status = itemissue.Status.Name;
+                                    string priority = itemissue.Priority.Name;
                                     if (BugNumber.ContainsKey(status + "," + priority))
                                     {
                                         BugNumber[status + "," + priority] += 1;
@@ -68,7 +81,7 @@ namespace AutoBugStatistics
                                     else
                                     {
                                         BugNumber.Add(status + "," + priority, 1);
-                                    }
+                                    }                                                                  
                                 }
                             }
                             string project_name = Regex.Replace(item.Name, @"\s", "");
@@ -141,142 +154,58 @@ namespace AutoBugStatistics
         public static string GetBugLog(Project project)
         {
             string result = string.Empty;
-            string str = string.Empty;
-            int newnum = GetTotalNumber("new");
-            int confirmednum = GetTotalNumber("confirmed");
-            int assignednum = GetTotalNumber("assigned");
-            int reopenednum = GetTotalNumber("reopened");
-            int reviewnum = GetTotalNumber("reviewing");
-            int resolvednum = GetTotalNumber("resolved");
-            int verifiednum = GetTotalNumber("verified");            
+            string str = string.Empty;   
             result +="<br>" + "Time:" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "<br>";
             result += GetParentList(project) + "<br>";
             result += "<table border=" + '"' + "1" + '"' +">";
+
+            int openBugs = 0;
+            int resolvedBugs = 0;
+            int verifiedBugs = 0;
+            foreach (var item in StatusList)
+            {                
+                if (item.Key == "Resolved" || item.Key == "Reviewing")
+                    resolvedBugs += GetTotalNumber(item.Key);
+                else if (item.Key == "Verified")
+                    verifiedBugs += GetTotalNumber(item.Key);
+                else 
+                    openBugs += GetTotalNumber(item.Key);
+            }            
             str = string.Format("OpenBug:{0}; ResolvedBug:{1}; VerifiedBug:{2}",
-                 newnum + confirmednum + assignednum + reopenednum, resolvednum + reviewnum, verifiednum);
-            result += "<th colspan=" + '"' + "8" + '"' + ">" + str + "</th>";
-            str = "<tr>" + AddHTMLoStr("", false) + AddHTMLoStr("New", true) +
-                  AddHTMLoStr("Comfirmed", true) + AddHTMLoStr("Assigned", true) +
-                  AddHTMLoStr("Reopened", true) + AddHTMLoStr("Reviewing", true) +
-                  AddHTMLoStr("Resolved", true) + AddHTMLoStr("Verified", true) + "</tr>";               
-            result += str;
-            str = "<tr>" + AddHTMLoStr("Low", true) +
-                  AddHTMLoStr(GetBugNumber("new,low").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("confirmed,low").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("assigned,low").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reopened,low").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reviewing,low").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("resolved,low").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("verified,low").ToString(), false) + "</tr>";                 
-            result += str;
-            str = "<tr>" + AddHTMLoStr("Normal", true) +
-                  AddHTMLoStr(GetBugNumber("new,normal").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("confirmed,normal").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("assigned,normal").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reopened,normal").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reviewing,normal").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("resolved,normal").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("verified,normal").ToString(), false) + "</tr>";                 
-            result += str ;
-            str = "<tr>" + AddHTMLoStr("High", true) +
-                  AddHTMLoStr(GetBugNumber("new,high").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("confirmed,high").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("assigned,high").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reopened,high").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reviewing,high").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("resolved,high").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("verified,high").ToString(), false) + "</tr>";                
-            result += str;
-            str = "<tr>" + AddHTMLoStr("Urgent", true) +
-                  AddHTMLoStr(GetBugNumber("new,Urgent").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("confirmed,Urgent").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("assigned,Urgent").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reopened,Urgent").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reviewing,Urgent").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("resolved,Urgent").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("verified,Urgent").ToString(), false) + "</tr>";                
-            result += str ;
-            str = "<tr>" + AddHTMLoStr("Immediate", true) +
-                  AddHTMLoStr(GetBugNumber("new,immediate").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("confirmed,immediate").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("assigned,immediate").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reopened,immediate").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("reviewing,immediate").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("resolved,immediate").ToString(), false) +
-                  AddHTMLoStr(GetBugNumber("verified,immediate").ToString(), false) + "</tr>";                
-            result += str ;
-            str = "<tr>" + AddHTMLoStr("Total", true) +
-                  AddHTMLoStr(newnum.ToString(), false) +
-                  AddHTMLoStr(confirmednum.ToString(), false) +
-                  AddHTMLoStr(assignednum.ToString(), false) +
-                  AddHTMLoStr(reopenednum.ToString(), false) +
-                  AddHTMLoStr(reviewnum.ToString(), false) +
-                  AddHTMLoStr(resolvednum.ToString(), false) +
-                  AddHTMLoStr(verifiednum.ToString(), false) + "</tr>";
-            result += str;
+                 openBugs, resolvedBugs, verifiedBugs);
+
+            result += "<th colspan=" + '"' + (StatusList.Count + 1) + '"' + ">" + str + "</th>";
+
+            result += "<tr>" + AddHTMLoStr("", false);
+            foreach (var item in StatusList)
+            {
+                result += AddHTMLoStr(item.Key, true);
+            }
+            result += "</tr>";
+
+            
+            foreach (var itemCol in PriorityList)
+            {
+                result += "<tr>" + AddHTMLoStr(itemCol.Key, true);
+                foreach (var itemRow in StatusList)
+                {
+                    result += AddHTMLoStr(GetBugNumber(itemRow.Key + "," + itemCol.Key).ToString(), false);
+
+                }
+                result += "</tr>";
+            }
+
+            result += "<tr>" + AddHTMLoStr("Total", true);
+            foreach (var item in StatusList)
+            {
+                result += AddHTMLoStr(GetTotalNumber(item.Key).ToString(), false);
+            }
+            result += "</tr>";  
+
             result += "</table>";
             return result;
-        }
-        //public static string GetBugLog(Project project)
-        //{
-        //    string result = string.Empty;
-        //    string str = string.Empty;
-        //    //string strFormat = "{0,10}{1,10}{2,10}{3,10}{4,10}{5,10}{6,10}{7,10}";
-        //    //string numFormat = "{0,10}{1,10:D}{2,10:D}{3,10:D}{4,10:D}{5,10:D}{6,10:D}{7,10:D}"; 
-        //    string strFormat = "{0}\t\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}";
-        //    string numFormat = "{0}\t{1:D}\t\t{2:D}\t\t{3:D}\t\t{4:D}\t\t{5:D}\t\t{6:D}\t\t{7:D}";
-        //    int newnum = GetTotalNumber("new");
-        //    int confirmednum = GetTotalNumber("confirmed");
-        //    int assignednum = GetTotalNumber("assigned");
-        //    int reopenednum = GetTotalNumber("reopened");
-        //    int reviewnum = GetTotalNumber("reviewing");
-        //    int resolvednum = GetTotalNumber("resolved");
-        //    int verifiednum = GetTotalNumber("verified");
-        //    result += "Time:" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "\r\n";
-        //    result += GetParentList(project) + "\r\n";
-        //    result += "*******************************************************************************************************************\r\n";
-        //    str = string.Format("OpenBug:{0}; ResolvedBug:{1}; VerifiedBug:{2}",
-        //         newnum + confirmednum + assignednum + reopenednum, resolvednum + reviewnum, verifiednum);
-        //    result += str + "\r\n";
-        //    str = string.Format(strFormat,
-        //        "    ", "New", "Confirmed", "Assigned", "Reopened", "Reviewing", "Resolved", "Verified");
-        //    result += str + "\r\n";
-        //    str = string.Format(numFormat,
-        //        "Low      ", GetBugNumber("new,low"), GetBugNumber("confirmed,low"),
-        //        GetBugNumber("assigned,low"), GetBugNumber("reopened,low"),
-        //        GetBugNumber("reviewing,low"), GetBugNumber("resolved,low"),
-        //        GetBugNumber("verified,low"));
-        //    result += str + "\r\n";
-        //    str = string.Format(numFormat,
-        //        "Normal   ", GetBugNumber("new,normal"), GetBugNumber("confirmed,normal"),
-        //        GetBugNumber("assigned,normal"), GetBugNumber("reopened,normal"),
-        //        GetBugNumber("reviewing,normal"), GetBugNumber("resolved,normal"),
-        //        GetBugNumber("verified,normal"));
-        //    result += str + "\r\n";
-        //    str = string.Format(numFormat,
-        //        "High     ", GetBugNumber("new,high"), GetBugNumber("confirmed,high"),
-        //        GetBugNumber("assigned,high"), GetBugNumber("reopened,high"),
-        //        GetBugNumber("reviewing,high"), GetBugNumber("resolved,high"),
-        //        GetBugNumber("verified,high"));
-        //    result += str + "\r\n";
-        //    str = string.Format(numFormat,
-        //        "Urgent   ", GetBugNumber("new,urgent"), GetBugNumber("confirmed,urgent"),
-        //        GetBugNumber("assigned,urgent"), GetBugNumber("reopened,urgent"),
-        //        GetBugNumber("reviewing,urgent"), GetBugNumber("resolved,urgent"),
-        //        GetBugNumber("verified,urgent"));
-        //    result += str + "\r\n";
-        //    str = string.Format(numFormat,
-        //        "Immediate", GetBugNumber("new,immediate"), GetBugNumber("confirmed,immediate"),
-        //        GetBugNumber("assigned,immediate"), GetBugNumber("reopened,immediate"),
-        //        GetBugNumber("reviewing,immediate"), GetBugNumber("resolved,immediate"),
-        //        GetBugNumber("verified,immediate"));
-        //    result += str + "\r\n";
-        //    str = string.Format(numFormat,
-        //        "Total    ", newnum, confirmednum, assignednum, reopenednum, reviewnum, resolvednum, verifiednum);
-        //    result += str + "\r\n";
-        //    result += "*******************************************************************************************************************\r\n";
-        //    return result;
-        //}
+        }      
+       
         public static int GetTotalNumber(string status)
         {
             int num = 0;
